0e3825c5aaeb3cf1449aceb941ccc030ad656f11
gb-titi <tristanmatteoli@gmail.com>
2023-04-13 12:05:54 +0200

    Mise à jour du back, documentation finie. Reste les services à mettre en place

--- src/keywords/dto/create-keyword.dto.ts
+++ src/keywords/dto/create-keyword.dto.ts
@@ -1,1 +1,21 @@
-export class CreateKeywordDto {}
+import { MinLength, MaxLength, Matches, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateKeywordDto {
+    @ApiProperty({ readOnly: true, type: String, description: "ID du mot-clé" })
+    @IsString()
+    id: string;
+  
+    @ApiProperty({ type: String, description: "Nom du mot-clé" })
+    @IsString()
+    @Matches(/^\b[a-zA-Z0-9_]+\b$/, {
+      message: "Attention, un seul mot est autorisé",
+    })
+    @MinLength(2, {
+      message: "Le mot doit faire au moins deux caractères.",
+    })
+    @MaxLength(15, {
+      message: "Le mot doit faire au maximum 15 caractères.",
+    })
+    name: string;
+  }
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,39 @@
-export class CreateTaskDto {}
+import {  MinLength, MaxLength, IsString, IsNotEmpty, IsArray, IsDateString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3, {
+    message: "Il faut au minimum trois caractères pour le titre."
+  })
+  @MaxLength(30, {
+    message: "Il faut au maximum 30 caractères pour le titre."
+  })
+  @ApiProperty({ type: String, description: 'Titre de la tâche' })
+  title: string;
+
+  @IsString()
+  @MaxLength(9999, {
+    message: "Votre tâche est beaucoup trop longue. Seulement 9999 caractères sont autorisés."
+  })
+  @ApiProperty({ type: String, description: 'Description de la tâche' })
+  task: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(120, {
+    message: "L'auteur à 120 caractères maximum autorisés."
+  })
+  @ApiProperty({ type: String, description: 'Auteur de la tâche' })
+  author: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, format: 'date', description: 'Date de création de la tâche' })
+  date: string;
+
+  @IsArray()
+  @ApiProperty({ type: [String], description: 'Mots-clefs associés à la tâche' })
+  keywords: string[];
+}
