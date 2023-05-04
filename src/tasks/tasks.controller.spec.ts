19171b6becf3eeb580d6351252cde8c2914b9892
GB-Titi <tristanmatteoli@gmail.com>
2023-05-04 12:45:11 +0200

    Résolution tests

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from './entities/task.entity';
 
 describe('TasksController', () => {
   let controller: TasksController;
 
   beforeEach(async () => {
+    const TaskModelMock: Partial<Model<Task>> = {};
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: getModelToken(Task.name),
+          useValue: TaskModelMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from './entities/task.entity';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(async () => {
+    const TaskModelMock: Partial<Model<Task>> = {};
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: getModelToken(Task.name),
+          useValue: TaskModelMock,
+        },
+      ],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
